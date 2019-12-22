cd404bf6b7a0cfdfc5168e2362ea3324c94c9058
AndrewAlan <andrewalangm@gmail.com>
2019-12-21 18:27:03 -0600

    Creating hash sha1 filter
    File writer module

--- lib/FileWriter.ex
+++ lib/FileWriter.ex
@@ -1,3 +1,5 @@
 defmodule FileWriter do
-
+  def write_file(hashes, pathfile) do
+    File.write(pathfile, hashes)
+  end
 end
--- lib/Hashing.ex
+++ lib/Hashing.ex
@@ -1,3 +1,7 @@
 defmodule Hashing do
-
+  def do_hash(lines) do
+    lines
+    |>
+    Enum.map(fn line -> :crypto.hash(:sha, line) end)
+  end
 end
